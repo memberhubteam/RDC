d7275c962599c666e667d437d9e451301866fd42
Aditya Kapoor <adityakapoor.mait@gmail.com>
2020-12-08 20:33:35 +0530

    Expose restaurant ID.

--- lib/RDC/catalog.rb
+++ lib/RDC/catalog.rb
@@ -16,35 +16,27 @@ module RDC
       end
 
       def download_xml_catalog
-        puts '[RDC] Start Downloading ZIP File from FTP Server.'
         Net::SFTP.start(RDC.ftp_url, RDC.ftp_username, password: RDC.ftp_password) do |sftp|
           sftp.download!('/rdc_MemberHub.zip', downloads_directory.join('rdc_MemberHub.zip').to_s)
         end
-        puts '[RDC] Finished Downloading ZIP File from FTP Server.'
       end
 
       def extract_zip_file
-        puts '[RDC] Starting Extracting XML File from ZIP File.'
         Zip::File.open(downloads_directory.join('rdc_MemberHub.zip')) do |zip_file|
           zip_file.each do |f|
             fpath = File.join(downloads_directory.to_s, f.name)
             zip_file.extract(f, fpath) unless File.exist?(fpath)
           end
         end
-        puts '[RDC] Done Extracting XML File from ZIP File.'
       end
 
       def parse_xml_file
-        puts '[RDC] Parsing XML'
         xml_file_path = downloads_directory.join('rdc_MemberHub.xml')
         @xml = Hash.from_xml(File.open(xml_file_path))
-        puts '[RDC] Finished parsing XML'
       end
 
       def process_xml_content
-        puts '[RDC] Starting XML Processing'
         @catalog = @xml["Catalogs"]["catalog"].collect { |catalog_hash| RDC::Restaurant.new(catalog_hash) }
-        puts '[RDC] Finished XML Processing'
         @catalog
       end
 
